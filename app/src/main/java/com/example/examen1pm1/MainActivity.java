4dcba1e25bd971a40ac6a7803670834d9f01f7c9
Alfaken90 <archaga.kevin@gmail.com>
2023-06-19 22:34:38 -0600

    Primer captura de foto

--- app/src/main/java/com/example/examen1pm1/MainActivity.java
+++ app/src/main/java/com/example/examen1pm1/MainActivity.java
@@ -2,14 +2,49 @@ package com.example.examen1pm1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCamara;
+    ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Estoy en Kevin_develop
+
+        btnCamara = findViewById(R.id.btnCamara);
+        imgView = findViewById(R.id.imgView);
+
+        btnCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirCamara();
+            }
+        });
+    }
+
+    private void abrirCamara() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 1);
+       // }
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            imgView.setImageBitmap(imgBitmap);
+        }
     }
 }
