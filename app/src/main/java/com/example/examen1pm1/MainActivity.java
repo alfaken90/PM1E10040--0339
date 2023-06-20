f7108180dedee14ca927b03f8eae226225b4295a
Alfaken90 <archaga.kevin@gmail.com>
2023-06-20 00:03:50 -0600

    Foto se guarda en el almacenamiento del telefono

--- app/src/main/java/com/example/examen1pm1/MainActivity.java
+++ app/src/main/java/com/example/examen1pm1/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.examen1pm1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnCamara;
     ImageView imgView;
+    String rutaImagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,28 +33,46 @@ public class MainActivity extends AppCompatActivity {
         imgView = findViewById(R.id.imgView);
         //kensyyyyyyyyyyyyyyy
 
-        btnCamara.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                abrirCamara();
-            }
-        });
+        btnCamara.setOnClickListener(v -> abrirCamara());
     }
 
     private void abrirCamara() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         //if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, 1);
-       // }
+
+            File imgArchivo = null;
+            try {
+                imgArchivo = crearImagen();
+            } catch (IOException ex) {
+                Log.e("Error", ex.toString());
+            }
+
+            if (imgArchivo != null) {
+                Uri fotoUri = FileProvider.getUriForFile(this,
+                        "com.example.examen1pm1.fileprovider", imgArchivo);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fotoUri);
+                startActivityForResult(intent, 1);
+            }
+
+        //}
 
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK){
-            Bundle extras = data.getExtras();
-            Bitmap imgBitmap = (Bitmap) extras.get("data");
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            //Bundle extras = data.getExtras();
+            Bitmap imgBitmap = BitmapFactory.decodeFile(rutaImagen);
             imgView.setImageBitmap(imgBitmap);
         }
+    }
+
+    private File crearImagen() throws IOException {
+        String nombreImagen = "foto_";
+        File directorio = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imgTemp = File.createTempFile(nombreImagen, ".jpg", directorio);
+
+        rutaImagen = imgTemp.getAbsolutePath();
+        return imgTemp;
     }
 }
