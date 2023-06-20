921b8c9d3bd5cea61f58937e291f51f491930557
Alfaken90 <archaga.kevin@gmail.com>
2023-06-19 21:27:55 -0600

    initial commit - En el branch Kevin_develop

--- app/src/main/java/com/example/examen1pm1/MainActivity.java
+++ app/src/main/java/com/example/examen1pm1/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Esta es una prueba
+        //Estoy en Kevin_develop
     }
 }
