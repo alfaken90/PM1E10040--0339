2c77b1f0eb85f6b059ad374c6d97deb3ccf4d124
Alfaken90 <archaga.kevin@gmail.com>
2023-06-22 08:52:18 -0600

    Diseño de la pantalla Editar

--- app/src/main/java/com/example/examen1pm1/EditarActivity.java
+++ app/src/main/java/com/example/examen1pm1/EditarActivity.java
@@ -2,13 +2,27 @@ package com.example.examen1pm1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditarActivity extends AppCompatActivity {
+
+    Button btnAtras3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar);
+
+        btnAtras3 = findViewById(R.id.btnAtras3);
+        btnAtras3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EditarActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
